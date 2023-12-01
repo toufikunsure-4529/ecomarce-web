be79579a6124b33b1a7838020bde86f9ca8ef43c
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-12-02 02:59:07 +0530

    add slider, product card ,footer,product summary view

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,48 @@ loginCard.addEventListener("mouseleave", () => {
   downArrow.style.display = "block";
   upArrow.style.display = "none"
 });
+
+
+
+//slider images 
+const slideContainer = document.querySelector(".slide-img");
+const images = document.querySelectorAll(".slide-img img");
+let currentIndex = 0;
+
+function showSlide(index) {
+  if (index < 0) {
+    currentIndex = images.length - 1;
+  } else if (index >= images.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
+  slideContainer.style.transform = `translateX(${-currentIndex * 100}%)`;
+
+  //dots current index active code
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("active-dot", index === currentIndex);
+  });
+
+}
+
+function goNext() {
+  showSlide(currentIndex + 1);
+}
+
+
+function goPrev() {
+  showSlide(currentIndex - 1);
+}
+
+setInterval(goNext, 5000);
+const dotsContainer = document.querySelector(".dots-container");
+for (let i = 0; i < images.length; i++) {
+  const dot = document.createElement("div")
+  dot.classList.add("dot")
+  dotsContainer.append(dot)
+  dot.addEventListener("click", () => showSlide(index))
+}
+
+
